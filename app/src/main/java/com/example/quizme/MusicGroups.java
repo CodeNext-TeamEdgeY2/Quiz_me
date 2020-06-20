f8f2045fdafa5f59be4af463ddb339d3d82fe8e2
Alexander <alexandera@csedge.org>
2020-06-20 12:14:54 -0700

    Original Title page and music groups page.

---
+++ app/src/main/java/com/example/quizme/MusicGroups.java
@@ -0,0 +1,14 @@
+package com.example.quizme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicGroups extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_groups);
+    }
+}
