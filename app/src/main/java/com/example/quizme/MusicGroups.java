23dc140e529dc97a8e3e55f2fc4bd25626359948
Alexander <alexandera@csedge.org>
2020-06-20 15:45:15 -0700

    Button Onclick added

--- app/src/main/java/com/example/quizme/MusicGroups.java
+++ app/src/main/java/com/example/quizme/MusicGroups.java
@@ -2,7 +2,9 @@ package com.example.quizme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MusicGroups extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MusicGroups extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_groups);
+    }
+
+    public void chalinosanchezquestions(View view) {
+        Intent newIntent = new Intent(this,ChalinoSanchezQuestions.class);
+        startActivity(newIntent);
     }
 }
