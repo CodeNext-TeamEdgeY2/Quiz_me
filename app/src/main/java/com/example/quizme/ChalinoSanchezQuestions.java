ee0675530deeb9843e405506a0144155e70a1226
Alexander <alexandera@csedge.org>
2020-06-20 15:25:33 -0700

    Chalino questions page added

---
+++ app/src/main/java/com/example/quizme/ChalinoSanchezQuestions.java
@@ -0,0 +1,14 @@
+package com.example.quizme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChalinoSanchezQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chalino_sanchez_questions);
+    }
+}
